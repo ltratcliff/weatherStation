513d1cfa56675459bca0c8a41ba35cb47539cf15
Tom Ratcliff <ltratcliff@gmail.com>
2021-08-09 19:56:53 -0400

    script to select today's weather from db and clean up date (convert from UTC to EST)

---
+++ today.py
@@ -0,0 +1,36 @@
+#!/opt/rh/rh-python38/root/bin/python3
+
+import datetime
+import sqlite3
+import pytz
+
+today = datetime.date.today()
+tomorrow = today + datetime.timedelta(days=1)
+est = pytz.timezone('US/Eastern')
+#fmt = '%Y-%m-%d %H:%M:%S %Z%z'
+fmt = '%H:%M:%S'
+
+s = f'''
+SELECT
+    Date,
+    Tempf,
+    FeelsLike,
+    Dailyrainin
+FROM
+    WEATHER
+WHERE
+    DATE BETWEEN '{today.isoformat()} 04:00:00+00:00' AND '{tomorrow.isoformat()} 04:00:00+00:00'
+ORDER BY
+    Date;
+'''
+
+
+conn = sqlite3.connect('weather.db')
+c = conn.cursor()
+
+c.execute(s)
+rows = c.fetchall()
+for row in rows:
+    dd = datetime.datetime.fromisoformat(row[0])
+    print(dd.astimezone(est).strftime(fmt), row[1], row[2], row[3])
+c.close()
